f9e390e5089bc25ef94832638374078b682ff0bd
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2026-08-29 21:27:58 +0300

    fix

--- .eleventy.mjs
+++ .eleventy.mjs
@@ -94,7 +94,7 @@ export default function (eleventyConfig) {
       data: `_data`,
       input: 'site',
       includes: `_includes`,
-      layouts: `_includes`,
+      layouts: 'site/_includes',
       output: '_site',
     },
   };
